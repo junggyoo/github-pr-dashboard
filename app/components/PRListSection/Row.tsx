01e074fdb0d70a2c129eaca27f4b50ba525ba690
SeungJu <asjj8051@gmail.com>
2025-02-01 15:29:16 +0900

    fix: 리뷰 완료 후 리스트 제거가 아니라 PR이 닫히면 리스트에 항목 제거로 수정

--- app/components/PRListSection/Row.tsx
+++ app/components/PRListSection/Row.tsx
@@ -1,7 +1,9 @@
 import { cn } from "~/lib/utils";
+import { PRListDto } from "~/service/usePullRequests";
+import { formatDate } from "~/utils/format";
 
 interface RowProps {
-  pr: any;
+  pr: PRListDto;
 }
 
 export default function Row({ pr }: RowProps) {
@@ -31,13 +33,13 @@ export default function Row({ pr }: RowProps) {
           <p>{pr.title}</p>
           <div className="flex gap-[6px] text-[12px] text-gray-500">
             <span>{pr.user.login}</span>
-            <span>{pr.repository.full_name}</span>
-            <span>{pr.created_at}</span>
+            <span>{pr.repository_url?.split("/repos/")[1] || ""}</span>
+            <span>{formatDate(pr.created_at)}</span>
           </div>
         </div>
       </div>
       <div className="flex gap-[8px]">
-        <Badge label="Pending" />
+        <Badge label="시작전" />
         <button
           className="bg-black text-white px-[12px] py-[4px] text-[12px] rounded-[8px] hover:bg-gray-600"
           onClick={() => handleReviewBtnClick(pr.html_url)}
--- app/service/usePullRequests.tsx
+++ app/service/usePullRequests.tsx
@@ -1,36 +1,56 @@
 import { Octokit } from "@octokit/rest";
 import { useEffect, useState } from "react";
 
+export interface PRListDto {
+  id: number;
+  number: number;
+  title: string;
+  html_url: string;
+  created_at: string;
+  user: {
+    login: string;
+    avatar_url: string;
+  };
+  repository_url: string;
+}
+
+export interface GitHubSearchResponse {
+  total_count: number;
+  incomplete_results: boolean;
+  items: PRListDto[];
+}
+
 const getOctokit = (token: string) =>
   new Octokit({
     auth: token,
   });
 
-const fetchPullRequests = async (token?: string) => {
-  if (!token) return [];
+const fetchPullRequests = async (
+  token?: string
+): Promise<GitHubSearchResponse | null> => {
+  if (!token) return null;
 
   const octokit = getOctokit(token);
 
   const { data } = await octokit.rest.search.issuesAndPullRequests({
-    q: "is:pr is:open review-requested:@me",
+    q: "is:pr is:open assignee:@me -author:@me",
     sort: "updated",
     order: "desc",
     per_page: 100,
   });
 
-  return data;
+  return data as GitHubSearchResponse;
 };
 
 export const usePullRequests = (token?: string) => {
-  const [prList, setPrList] = useState<any[]>([]);
+  const [prList, setPrList] = useState<PRListDto[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const updatePrList = async () => {
     try {
       setIsLoading(true);
       const data = await fetchPullRequests(token);
-      console.log("data", data);
-      setPrList(data.items);
+      setPrList(data?.items || []);
     } catch (error) {
       console.error("Error fetching pull requests", error);
     } finally {
