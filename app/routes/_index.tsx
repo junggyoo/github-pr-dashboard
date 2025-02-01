815d4c60de649c423881d3ce2aa81cc9d3450d56
playck <playck@naver.com>
2025-02-01 18:33:02 +0900

    test test

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,73 +10,73 @@ import { getSession } from "~/utils/session.server";
 import { getPullRequests } from "~/services/github.server";
 
 export const meta: MetaFunction = () => {
-	return [
-		{ title: "PR Dashboard" },
-		{ name: "description", content: "Github PR Dashboard" },
-	];
+  return [
+    { title: "PR Dashboard" },
+    { name: "description", content: "Github PR Dashboard" },
+  ];
 };
-
+console.log("d");
 export const loader: LoaderFunction = async ({ request }) => {
-	const session = await getSession(request);
-	const token = session.get("token");
-	const url = new URL(request.url);
-	const prStatus = url.searchParams.get("prStatus") || "all";
+  const session = await getSession(request);
+  const token = session.get("token");
+  const url = new URL(request.url);
+  const prStatus = url.searchParams.get("prStatus") || "all";
 
-	if (!token) {
-		return redirect("/auth/github");
-	}
+  if (!token) {
+    return redirect("/auth/github");
+  }
 
-	const { prList, assignedPRCount, totalPRCount } = await getPullRequests(
-		token,
-		prStatus as PRStatus
-	);
+  const { prList, assignedPRCount, totalPRCount } = await getPullRequests(
+    token,
+    prStatus as PRStatus
+  );
 
-	return {
-		prList,
-		prStatus,
-		나에게_할당된_PR_개수: assignedPRCount,
-		총_PR_개수: totalPRCount,
-	};
+  return {
+    prList,
+    prStatus,
+    나에게_할당된_PR_개수: assignedPRCount,
+    총_PR_개수: totalPRCount,
+  };
 };
 
 export default function Index() {
-	const navigate = useNavigate();
-	const navigation = useNavigation();
-	const {
-		prList,
-		prStatus: initialStatus,
-		나에게_할당된_PR_개수,
-		총_PR_개수,
-	} = useLoaderData<typeof loader>();
-	const [prStatus, setPrStatus] = useState<PRStatus>(initialStatus);
+  const navigate = useNavigate();
+  const navigation = useNavigation();
+  const {
+    prList,
+    prStatus: initialStatus,
+    나에게_할당된_PR_개수,
+    총_PR_개수,
+  } = useLoaderData<typeof loader>();
+  const [prStatus, setPrStatus] = useState<PRStatus>(initialStatus);
 
-	const handleStatusChange = (prStatus: PRStatus) => {
-		setPrStatus(prStatus);
-		navigate(`?prStatus=${prStatus}`);
-	};
+  const handleStatusChange = (prStatus: PRStatus) => {
+    setPrStatus(prStatus);
+    navigate(`?prStatus=${prStatus}`);
+  };
 
-	const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === "loading";
 
-	return (
-		<div className="flex flex-col gap-[32px]">
-			<section className="flex gap-5">
-				<div className="flex-1 rounded-2xl bg-white p-6 shadow-[0px_1px_3px_0px_rgba(0,0,0,0.1),0px_1px_2px_-1px_rgba(0,0,0,0.1)]">
-					<div className="mb-6 text-sm font-medium text-gray-500">
-						🚨 나의 승인이 필요한 PR
-					</div>
-					<div className="text-2xl font-semibold">
-						{나에게_할당된_PR_개수}개
-					</div>
-				</div>
-				<div className="flex-1 rounded-2xl bg-white p-6 shadow-[0px_1px_3px_0px_rgba(0,0,0,0.1),0px_1px_2px_-1px_rgba(0,0,0,0.1)]">
-					<div className="mb-6 text-sm font-medium text-gray-500">
-						🔥 내가 리뷰해야 할 PR
-					</div>
-					<div className="text-2xl font-semibold">{총_PR_개수}개</div>
-				</div>
-			</section>
-			<Filter prStatus={prStatus} onStatusChange={handleStatusChange} />
-			{isLoading ? <div>Loading...</div> : <PRListSection prList={prList} />}
-		</div>
-	);
+  return (
+    <div className="flex flex-col gap-[32px]">
+      <section className="flex gap-5">
+        <div className="flex-1 rounded-2xl bg-white p-6 shadow-[0px_1px_3px_0px_rgba(0,0,0,0.1),0px_1px_2px_-1px_rgba(0,0,0,0.1)]">
+          <div className="mb-6 text-sm font-medium text-gray-500">
+            🚨 나의 승인이 필요한 PR
+          </div>
+          <div className="text-2xl font-semibold">
+            {나에게_할당된_PR_개수}개
+          </div>
+        </div>
+        <div className="flex-1 rounded-2xl bg-white p-6 shadow-[0px_1px_3px_0px_rgba(0,0,0,0.1),0px_1px_2px_-1px_rgba(0,0,0,0.1)]">
+          <div className="mb-6 text-sm font-medium text-gray-500">
+            🔥 내가 리뷰해야 할 PR
+          </div>
+          <div className="text-2xl font-semibold">{총_PR_개수}개</div>
+        </div>
+      </section>
+      <Filter prStatus={prStatus} onStatusChange={handleStatusChange} />
+      {isLoading ? <div>Loading...</div> : <PRListSection prList={prList} />}
+    </div>
+  );
 }
