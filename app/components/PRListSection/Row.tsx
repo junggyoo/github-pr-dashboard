f88d34e429d193ffb800a345164ae1cf346b0358
SeungJu <asjj8051@gmail.com>
2025-02-01 12:36:43 +0900

    feat: PR 리스트 테이블 UI 개발완료

--- app/components/PRListSection/Row.tsx
+++ app/components/PRListSection/Row.tsx
@@ -5,11 +5,16 @@ interface RowProps {
 }
 
 export default function Row({ pr }: RowProps) {
+  const handleReviewBtnClick = (url: string) => {
+    window.open(url, "_blank");
+  };
+
   return (
     <div
       className={cn(
         "flex justify-between items-center",
-        "bg-white border border-gray-200 rounded-[8px] p-[12px]"
+        "bg-white border border-gray-200 rounded-[8px] p-[12px]",
+        "hover:bg-gray-100"
       )}
     >
       <div className="flex gap-[8px]">
@@ -31,7 +36,24 @@ export default function Row({ pr }: RowProps) {
           </div>
         </div>
       </div>
-      <div></div>
+      <div className="flex gap-[8px]">
+        <Badge label="Pending" />
+        <button
+          className="bg-black text-white px-[12px] py-[4px] text-[12px] rounded-[8px] hover:bg-gray-600"
+          onClick={() => handleReviewBtnClick(pr.html_url)}
+          type="button"
+        >
+          리뷰하기
+        </button>
+      </div>
     </div>
   );
 }
+
+const Badge = ({ label }: { label: string }) => {
+  return (
+    <div className="flex items-center bg-gray-100 rounded-full px-[8px] text-[10px] text-gray-500">
+      {label}
+    </div>
+  );
+};
