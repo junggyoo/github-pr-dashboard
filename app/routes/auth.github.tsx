8b640f0161f2ba81cf564b8b5bff63951fbb964f
SeungJu <asjj8051@gmail.com>
2025-02-09 21:58:21 +0900

    fix: 깃헙 토큰 scope 수정

--- app/routes/auth.github.tsx
+++ app/routes/auth.github.tsx
@@ -2,13 +2,13 @@ import { redirect } from "@remix-run/node";
 import { getBaseUrl } from "~/utils/env";
 
 export async function loader() {
-	const clientId = process.env.GITHUB_CLIENT_ID;
-	const scope = "repo user"; // 필요한 권한 스코프
+  const clientId = process.env.GITHUB_CLIENT_ID;
+  const scope = "repo user read:org pull_requests";
 
-	const baseUrl = getBaseUrl();
-	const redirectUri = `${baseUrl}/auth/github/callback`;
+  const baseUrl = getBaseUrl();
+  const redirectUri = `${baseUrl}/auth/github/callback`;
 
-	return redirect(
-		`https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}`
-	);
+  return redirect(
+    `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}`
+  );
 }
